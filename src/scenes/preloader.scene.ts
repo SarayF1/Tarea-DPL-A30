6f97f116d5a344532807f84258ad4beaaec6e6b6
SarayF1 <saraytatiana666@gmail.com>
2026-03-18 17:22:13 +0000

    Cambios 7

--- src/scenes/preloader.scene.ts
+++ src/scenes/preloader.scene.ts
@@ -8,12 +8,14 @@ export class PreloaderScene extends Scene {
 
     preload(): void {
         Logger.info('Cargando assets...');
+
         this.load.image('acho', 'acho.png');
         this.load.image('fondo', 'fondo.png');
     }
 
     create(): void {
         Logger.info('PreloaderScene finalizada');
+
         this.scene.start('game');
     }
 }
